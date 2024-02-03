ce882883065f9933ec700e06b5966415f2d92546
oleg-olegi <ol.shink87@gmail.com>
2024-02-03 14:24:35 +0300

    1.4 Научиться реагировать на команду /start и выводить приветственное сообщение

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,21 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            if (update.message() != null && "/start".equals(update.message().text())) {
+                long chatId = update.message().chat().id();
+                sendWelcomeMessage(chatId);
+            }
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void sendWelcomeMessage(long chatId) {
+        //приветственное сообщение
+        String welcomeMessage = "Привет! Добро пожаловать! Я ваш бот. Рад видеть вас!";
+        //отправка сообщения в чат
+        telegramBot.execute(new SendMessage(chatId, welcomeMessage));
+        logger.info("Sending welcome message to chat {}: {}", chatId, welcomeMessage);
     }
 
 }
