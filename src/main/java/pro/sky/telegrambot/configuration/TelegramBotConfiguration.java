ab9110410f3722801a91fefd2ebdaa9f503258fe
oleg-olegi <ol.shnk87@gmail.com>
2024-02-11 21:46:46 +0300

    2.6 Научиться для выбранных записей рассылать уведомления

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -12,6 +12,7 @@ public class TelegramBotConfiguration {
     @Value("${telegram.bot.token}")
     private String token;
 
+
     @Bean
     public TelegramBot telegramBot() {
         TelegramBot bot = new TelegramBot(token);
